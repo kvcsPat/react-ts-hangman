51be2424cd0450db5d80e53a67743112c04d2a77
kvcsPat <smth.patrik@gmail.com>
2024-04-17 13:53:03 +0200

    created HangmanWord component

--- hangman/src/Components/HangmanWord.tsx
+++ hangman/src/Components/HangmanWord.tsx
@@ -1,1 +1,31 @@
-export function HangmanWord() {}
+export function HangmanWord() {
+  const word = "test";
+  const guessedLetters = ["t"];
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: ".25em",
+        fontFamily: "monospace",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+      }}
+    >
+      {word.split("").map((letter, index) => (
+        <span style={{ borderBottom: ".1em solid black" }} key={index}>
+          <span
+            style={{
+              visibility: guessedLetters.includes(letter)
+                ? "visible"
+                : "hidden",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
+}

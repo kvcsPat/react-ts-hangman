73a702686fb92472c448a98a51b9c5ccf5fd08bb
kvcsPat <smth.patrik@gmail.com>
2024-04-17 13:13:28 +0200

    created HangmanDrawing component

--- hangman/src/App.tsx
+++ hangman/src/App.tsx
@@ -1,5 +1,33 @@
+import { useState } from "react";
+import words from "./wordList.json";
+import { HangmanDrawing } from "./Components/HangmanDrawing";
+import { HangmanWord } from "./Components/HangmanWord";
+import { Keyboard } from "./Components/Keyboard";
+
 function App() {
-  return <h1>Hi</h1>;
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        maxWidth: "800px",
+        margin: "0 auto",
+        gap: "2rem",
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose Win</div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
+  );
 }
 
 export default App;
---
+++ hangman/src/Components/HangmanDrawing.tsx
@@ -0,0 +1,126 @@
+const HEAD = (
+  <div
+    style={{
+      position: "absolute",
+      top: "50px",
+      right: "-30px",
+      width: "50px",
+      height: "50px",
+      borderRadius: "100%",
+      border: "10px solid black",
+    }}
+  />
+);
+
+const BODY = (
+  <div
+    style={{
+      position: "absolute",
+      top: "120px",
+      right: "0",
+      width: "10px",
+      height: "100px",
+      background: "black",
+    }}
+  />
+);
+
+const RIGHT_ARM = (
+  <div
+    style={{
+      position: "absolute",
+      top: "150px",
+      right: "-100px",
+      width: "100px",
+      height: "10px",
+      rotate: "-30deg",
+      transformOrigin: "left bottom",
+      background: "black",
+    }}
+  />
+);
+
+const LEFT_ARM = (
+  <div
+    style={{
+      position: "absolute",
+      top: "150px",
+      right: "10px",
+      width: "100px",
+      height: "10px",
+      rotate: "30deg",
+      transformOrigin: "right bottom",
+      background: "black",
+    }}
+  />
+);
+
+const RIGHT_LEG = (
+  <div
+    style={{
+      position: "absolute",
+      top: "210px",
+      right: "-90px",
+      width: "100px",
+      height: "10px",
+      rotate: "60deg",
+      transformOrigin: "left bottom",
+      background: "black",
+    }}
+  />
+);
+
+const LEFT_LEG = (
+  <div
+    style={{
+      position: "absolute",
+      top: "210px",
+      right: "0",
+      width: "100px",
+      height: "10px",
+      rotate: "-60deg",
+      transformOrigin: "right bottom",
+      background: "black",
+    }}
+  />
+);
+
+export function HangmanDrawing() {
+  return (
+    <div style={{ position: "relative" }}>
+      {HEAD}
+      {BODY}
+      {RIGHT_ARM}
+      {LEFT_ARM}
+      {RIGHT_LEG}
+      {LEFT_LEG}
+      <div
+        style={{
+          position: "absolute",
+          top: "0",
+          right: "0",
+          height: "50px",
+          width: "10px",
+          background: "black",
+        }}
+      />
+      <div
+        style={{
+          height: "10px",
+          width: "200px",
+          marginLeft: "120px",
+          background: "black",
+        }}
+      />
+      <div
+        style={{
+          height: "400px",
+          width: "10px",
+          marginLeft: "120px",
+          background: "black",
+        }}
+      />
+      <div style={{ height: "10px", width: "250px", background: "black" }} />
+    </div>
+  );
+}
